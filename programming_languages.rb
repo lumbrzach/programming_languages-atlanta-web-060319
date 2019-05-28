8bd2dd85902b8c90ea7fb4ad91598e90bab9097b
Zach Parker <parkerz410@gmail.com>
2019-05-28 14:16:16 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,56 @@
+require "pry"
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
+
+
 def reformat_languages(languages)
-  # your code here
+  masterhash = {}
+  languages.map do |style, language|
+    # binding.pry
+    language.map do |language, info|
+      # binding.pry
+      if style == :oo && style == :functional
+        info[:style] = [:oo, :functional]
+      elsif style == :oo
+        info[:style] = [:oo]
+      else style == :functional
+        info[:style] = [:functional]
+      end
+      masterhash[language] = info
+      # binding.pry
+    end
+  end
+  # binding.pry
+  return masterhash
+# binding.pry
 end
